97701979adcf04616b1cb688b70649cc0b0ae3e7
junio10 <junio.js977@gmail.com>
2024-09-25 20:00:34 -0300

    adicionando arquivos com praticas do fundamentos

--- App.tsx
+++ App.tsx
@@ -4,17 +4,31 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={stylesText.container}>hello world!</Text>
+      <Text style={{
+        color: 'yellow',
+        fontSize: 21,
+        padding:19
+      }}>Learn react native</Text>
       <StatusBar style="auto" />
     </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'blue',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+ 
+const stylesText = StyleSheet.create({
+  container:{
+    color: 'red',
+    fontSize: 45,
+    fontStyle:"italic",
+  }
+});  

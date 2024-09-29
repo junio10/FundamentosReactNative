f46262151c966245d143311e1b35afca2087b9fa
junio10 <junio.js977@gmail.com>
2024-09-28 23:23:36 -0300

    adicionando input e botao

--- src/screens/home/home.tsx
+++ src/screens/home/home.tsx
@@ -1,17 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, TextInput, View } from 'react-native';
+import { Text, TextInput, View, Touchable, TouchableOpacity } from 'react-native';
 import { styles } from './Style';
-import { StyleSheet } from "react-native";
 
 export function Home() {
+  let a = 0;
+  function handlerParticipantAdd(){
+    a+=1;
+    console.log(a);
+    
+  }
   return (
     <View style={styles.container}>
       <Text style={styles.textName}>Nome do evento:</Text>
+
       <TextInput 
-      style= {styles.TextInput}
+      style= {styles.textInput}
       placeholder="Nome do evento"
       placeholderTextColor='#6B6B6B'
       />
+
+      <TouchableOpacity style={styles.button} onPress={handlerParticipantAdd}>
+        <Text style={styles.textButton}>
+          +
+        </Text>
+      </TouchableOpacity>
+    
     </View>
     
   );

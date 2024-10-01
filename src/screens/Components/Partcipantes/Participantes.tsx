7daf37a3366206d44e8648cb46db7d8a7e40af6c
junio10 <junio.js977@gmail.com>
2024-09-30 22:46:00 -0300

    adicionando pasta de componentes criados

---
+++ src/screens/Components/Partcipantes/Participantes.tsx
@@ -0,0 +1,10 @@
+import React from 'react'; // Explicitly import React
+import { View, Text } from 'react-native';
+
+export function Participantes() {
+    return (
+        <View>
+            <Text>Lista de Participantes</Text>
+        </View>
+    );
+}

532fd37e787d9cbd0f130a31c53f42df50dd7555
junio10 <junio.js977@gmail.com>
2024-09-30 22:44:44 -0300

    adicionando form

--- src/screens/home/home.tsx
+++ src/screens/home/home.tsx
@@ -3,28 +3,28 @@ import { styles } from './Style';
 
 export function Home() {
   let a = 0;
-  function handlerParticipantAdd(){
-    a+=1;
+  function handlerParticipantAdd() {
+    a += 1;
     console.log(a);
-    
+
   }
   return (
     <View style={styles.container}>
       <Text style={styles.textName}>Nome do evento:</Text>
+      <View style={styles.form}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Nome do evento"
+          placeholderTextColor='#6B6B6B'
+        />
 
-      <TextInput 
-      style= {styles.textInput}
-      placeholder="Nome do evento"
-      placeholderTextColor='#6B6B6B'
-      />
-
-      <TouchableOpacity style={styles.button} onPress={handlerParticipantAdd}>
-        <Text style={styles.textButton}>
-          +
-        </Text>
-      </TouchableOpacity>
-    
+        <TouchableOpacity style={styles.button} onPress={handlerParticipantAdd}>
+          <Text style={styles.textButton}>
+            +
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
-    
+
   );
 }
